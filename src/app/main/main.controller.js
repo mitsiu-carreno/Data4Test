ca37523331b14f754a3905bba967aa35f92e2fff
mitsiu <mitsiu.carreno@gmail.com>
2017-01-28 10:53:31 -0600

    Add sort functionality

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -17,12 +17,11 @@
     vm.selectedState = 1;   //Default state selected
     vm.selectedOrder = vm.orderOptions[0].id;       //Default order selected
     vm.dataFilteredByYear = [];   //Array with idh only from the year selected
-    vm.averageIdh = 0;
-    vm.highestIdh = 0;
-    vm.lowestIdh = 1;
+
 
     vm.createData = createData;
-    vm.renderGraph = renderGraph;
+    vm.getDataGraph = getDataGraph;
+    vm.sortDataGraph = sortDataGraph
 
 
     //Ajax to get the list of states
@@ -31,7 +30,6 @@
       createData();
     }).error(function(response, status, headers, config){
       console.error(response);
-      vm.lowestIdh = 0; //If theres no states, you can't calc the lowest, default 0
       alert('Desapareció la nación!');
     });
 
@@ -42,41 +40,67 @@
       //Foreach year generate random idh by state
       for(var year=0; year<=years; year++){
         var newIdhEntry = {
-          year    : vm.currentYear - year ,
-          states  : angular.copy(vm.states)
+          year        : vm.currentYear - year ,
+          states      : angular.copy(vm.states),
+          averageIdh  : 0,
+          lowestIdh   : 1,
+          highestIdh  : 0
         };
 
+
+        var idhSum = 0; //summation of idh
         //Foreach state generate random IDH between 0-1 with 2 decimals
         angular.forEach(newIdhEntry.states, function(state, key) {
           state.idh = helper.generateRandom(0, 1, 2);
+
+          //Calculate the average, lowest and highest idh from selected year
+          idhSum += parseFloat(state.idh);
+          if(state.idh > newIdhEntry.highestIdh)
+            newIdhEntry.highestIdh = state.idh;
+          if(state.idh < newIdhEntry.lowestIdh)
+            newIdhEntry.lowestIdh = state.idh;
         });
+        //Calc average (summation / length)
+        newIdhEntry.averageIdh = idhSum/newIdhEntry.states.length;
+
         //Add the new entry to the data
         vm.data.push(newIdhEntry);
       }
-      renderGraph();
+      getDataGraph();
+      sortDataGraph();
     }
 
     // Separates only the data from the year selected
-    function renderGraph (){
+    function getDataGraph (){
       angular.forEach(vm.data, function(yearEntry, key){
         if(yearEntry.year == vm.selectedYear){
-          console.log(yearEntry.states);
-          vm.dataFilteredByYear = angular.copy(yearEntry.states);
-
-          var idhSum = 0; //
-          //Calculate the average, lowest and highest idh from selected year
-          angular.forEach(yearEntry.states, function(stateData, key){
-            idhSum += parseFloat(stateData.idh);
-            if(stateData.idh > vm.highestIdh)
-              vm.highestIdh = stateData.idh;
-            if(stateData.idh < vm.lowestIdh)
-              vm.lowestIdh = stateData.idh;
-          });
-          //Calc average (summation / length)
-          console.log(idhSum);
-          vm.averageIdh = idhSum/yearEntry.states.length;
+          vm.dataFilteredByYear = angular.copy(yearEntry);
         }
       });
+    }
+
+    //Sort the data according to input
+    function sortDataGraph (){
+      if(vm.selectedOrder == "A"){        //Ascending
+        vm.dataFilteredByYear.states.sort(function(a, b){
+          return a.idh - b.idh;
+        });
+      }else if(vm.selectedOrder == "D"){  //Decending
+        vm.dataFilteredByYear.states.sort(function(a, b){
+          return b.idh - a.idh;
+        });
+      }else{                              //Alphabetical
+        vm.dataFilteredByYear.states.sort(function(a, b){
+          var nameA = a.fullName.toLowerCase(), nameB = b.fullName.toLowerCase()
+          if(nameA < nameB){
+            return -1;
+          }else if(nameA > nameB){
+            return 1;
+          }else {
+            return 0
+          }
+        });
+      }
     }
 
     var svg = d3.select("svg"),
