73b042dd954e68e631340741f262b732e969d775
mitsiu <mitsiu.carreno@gmail.com>
2017-01-27 18:51:25 -0600

    Comment code

--- src/app/components/helper/helper.service.js
+++ src/app/components/helper/helper.service.js
@@ -6,8 +6,16 @@
     .service('helper', helper);
 
     function helper() {
+
       this.generateRandom = generateRandom;
 
+      /*
+      This function generates a random int or float depending on the number of decimals
+      int min -> minimum number
+      int max -> maximum number
+      int decimals -> 0 outputs Int
+                   -> 0 outputs float
+      */
       function generateRandom(min, max, decimals){
         var newRandom = (Math.random() * (max - min) + min).toFixed(decimals);
         return newRandom;
