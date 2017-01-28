d4348f4acbb987c6a65a5d58b5e30f353bf1ece8
mitsiu <mitsiu.carreno@gmail.com>
2017-01-27 21:57:42 -0600

    Start to create svg and read data at directive

--- src/app/components/d3/d3BarChart.directive.js
+++ src/app/components/d3/d3BarChart.directive.js
@@ -22,7 +22,32 @@
       return directive;
 
       function graphLink(scope, el, attr, vm){
-        console.log(scope);
+
+        //Create an svg tag inside element
+        var svg = d3.select(el[0])
+          .append('svg')
+          .attr("width", "100%")
+          .attr("height", "100%");
+
+        var margin = {top:20, right:20, bottom:30, left:40},
+          width  =+ svg.attr('width') - margin.left - margin.right,
+          height =+ svg.attr('height') - margin.top - margin.bottom;
+
+        var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+            y = d3.scaleLinear().rangeRound([height, 0]);
+
+        var g = svg.append('g')
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        d3.tsv("../app/components/fixedData.tsv", function(d){
+          d.frequency =+ d.frequency;
+          return d;
+        }, function(err, data){
+          if(err){
+            console.error("Err while reading tvs" + err);
+            throw err;
+          }
+        })
       }
 
       function idhGraphController(){
